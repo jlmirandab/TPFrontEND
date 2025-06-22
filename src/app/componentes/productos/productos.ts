944b125e4ca0f1d1757729ca06a0bc0ae2e0a117
jlmirandab <jorgemiranda2108@gmail.com>
2025-06-21 23:26:39 -0300

    Inicio terminado
    Ignorar el nombre de la rama, se actualizo "Inicio"

--- src/app/componentes/productos/productos.ts
+++ src/app/componentes/productos/productos.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { Drink, DrinksResponse } from '../../models/product.model';
+import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Cocktail } from '../../servicios/cocktail';
 
 @Component({
   selector: 'app-productos',
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './productos.html',
   styleUrl: './productos.css'
 })
-export class Productos {
+export class Productos implements OnInit {
+ currentDrink: Drink | null = null;  // Inicializada como null
+ isLoading: boolean = false;   
+  listadeTragos: Drink[]=[];
 
+   constructor(private cocktailService: Cocktail) {}
+
+  ngOnInit(): void {
+    
+    
+  }
+
+//*
+//     getTragoID(id: string) {
+//       this.cocktailService.getTragoByID(id).subscribe({
+//         next: (data: any) => {
+//       this.listadeTragos = data;
+//         console.log('Trago por ID:', this.listadeTragos);
+//     },
+//     error: (error: any) => {
+//       console.error('Error buscando trago por ID', error);
+//     }
+//   });
+// }**/
 }
--- src/app/servicios/cocktail.ts
+++ src/app/servicios/cocktail.ts
@@ -1,17 +1,39 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-
+import { forkJoin, map, Observable, of, switchMap } from 'rxjs';
+import { Drink, DrinksResponse } from '../models/product.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class Cocktail {
-   private apiUrl = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail';
+   private Url = 'https://www.thecocktaildb.com/api/json/v1/1';
 
-  constructor(private http: HttpClient) {}
+  constructor(private _httpClient: HttpClient) {}
+  
 
-  getCocktails(): Observable<any> {
-    return this.http.get(this.apiUrl);
+      public getDrinksByLetter(letter: string): Observable<any[]> {
+    return this._httpClient.get<any>(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${letter}`).pipe(
+      map(res => res.drinks || [])
+    );
+  }
+
+    public getDrinkById(id: string): Observable<Drink | null> {
+    return this._httpClient.get<DrinksResponse>(`${this._httpClient}/lookup.php?i=${id}`).pipe(
+      map(res => res.drinks ? res.drinks[0] : null)
+    );
+  }
+
+    public searchDrinksByName(name: string): Observable<Drink[]> {
+    return this._httpClient.get<DrinksResponse>(`${this._httpClient}/search.php?s=${name}`).pipe(
+      map(res => res.drinks || [])
+    );
+  }
+
+  getRandomDrink(): Observable<any> {
+    return this._httpClient.get(`${this.Url}/random.php`).pipe(
+      map((response: any) => response.drinks[0]) // Extrae el primer trago
+    );
+  }
+
   } 
-}
